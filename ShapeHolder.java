2159421985206eda7b50e63fdd2247db29d3091a
Griffin Noe <steelersfan437@gmail.com>
2019-11-04 11:47:56 -0500

    Create ShapeHolder
    Created the shapeholder class.

---
+++ ShapeHolder.java
@@ -0,0 +1,13 @@
+public class ShapeHolder implements Iterable<Shape>{
+    public ShapeHolder(){
+        Shape[] holder = new  Shape[50];
+    }
+
+    public boolean hasNext(){
+
+    }
+
+    public T next(){
+        
+    }
+}
