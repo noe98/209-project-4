9115986f44411d80595460c2bbe9e13481ec554b
Griffin Noe <steelersfan437@gmail.com>
2019-11-09 16:23:54 -0500

    Integrated ShapeHolderIterator
    Integrated shapeholderiterator into shapeholder

--- ShapeHolder.java
+++ ShapeHolder.java
@@ -1,7 +1,58 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+// import shapes
 
 public class ShapeHolder implements Iterable<Shape>{
-    public Iterator<> iterator(){
+    Shape[] shapes;
+    int counter = 0;
+    private static ShapeHolder inst;
+
+    private ShapeHolder(){
+        this.shapes = new Shapes[50];
+    }
+
+    public static ShapeHolder getInstance(){
+        if(inst == null){
+            inst = new ShapeHolder();
+        }
+        return inst;
+    }
+
+    public ShapeHolderIterator<> iterator(){
         return new ShapeHolderIterator<>(this);
+    }
+
+    public void addShape(Shape shape){
+        if(counter < 50){
+            this.shapes[counter] = shape;
+            counter++;
+        }
+    }
+
+    public void removeShape(){
+        if(counter > 0){
+            this.shapes[counter] = null;
+            counter--;
+        }   
+    }
+}
+
+public class ShapeHolderIterator implements Iterator<Shape>{
+    int current = 0;
+    ShapeHolder sh;
+
+    public ShapeHolderIterator(ShapeHolder sh){
+        this.sh = sh;
+    }
+
+    public boolean hasNext(){
+        return (current < sh.counter);
+    }
+
+    public Shape next(){
+        if(!hasNext()){
+            throw new NoSuchElementException();
+        }
+        return sh.shapes[current++];
     }
 }
